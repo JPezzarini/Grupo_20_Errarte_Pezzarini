8178c3b2811f8ec691d26c926008d4be78362504
JPezzarini <juanpezzarini1@gmail.com>
2025-04-28 18:59:54 -0300

    Cambios en sistema Mateo

--- sistema.py
+++ sistema.py
@@ -32,11 +32,36 @@ class Sistema():
         self.lista_receptores.append(receptor)
     
 
-    def elegir_receptor(self, receptores: list):
-        """La lista de receptores tiene todos los receptores compatibles que necesiten el órgano especificado
-            Esta lista fue hecha en la funcion buscar match
-        """
     def elegir_receptor_prioridad(self, receptores: list):
         """
         bubble sort
         """
+        for i in range(len(receptores)):
+            for k in range (0,len(receptores)-1-i):
+                if(receptores[k+1].dt_espera > receptores[k].dt_espera and receptores[k+1].prioridad == receptores[k].prioridad):
+                    a = receptores[k]
+                    receptores[k] = receptores[k+1]
+                    receptores[k+1] = a
+
+
+    def elegir_receptor(self, receptores: list):
+        """La lista de receptores tiene todos los receptores compatibles que necesiten el órgano especificado
+            Esta lista fue hecha en la funcion buscar match
+        """
+        for i in range(len(receptores)):
+            for k in range (0,len(receptores)-1-i):
+                if(receptores[k+1].prioridad > receptores[k].prioridad):
+                    a = receptores[k]
+                    receptores[k] = receptores[k+1]
+                    receptores[k+1] = a
+        self.elegir_receptor_prioridad(receptores)
+
+
+    def buscar_match(self,donante: Donante,organo: Organo):
+        receptores = []
+        for i in range(len(self.lista_receptores)):
+            if(self.lista_receptores[i].organo_r.__tipo == organo.__tipo and self.lista_receptores[i]._t_sangre == donante._t_sangre):
+                receptores.append(self.lista_receptores[i])
+        self.elegir_receptor(receptores)
+        
+
